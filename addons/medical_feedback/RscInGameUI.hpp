5d16b6e246e8a8d3c30b61d96eea7e4140514b53
Salluci <69561145+Salluci@users.noreply.github.com>
2021-12-24 09:40:46 -0300

    remove medical feedback changes

--- addons/medical_feedback/RscInGameUI.hpp
+++ addons/medical_feedback/RscInGameUI.hpp
@@ -1,7 +1,6 @@
 class RscPictureKeepAspect;
 class RscInGameUI {
-    class RscUnitInfo;
-    class RscUnitInfoSoldier: RscUnitInfo { // Kept here to be (generally) difficulty independent, uses stance macros for placement
+    class RscStanceInfo { // Kept here to be (generally) difficulty independent, uses stance macros for placement
          controls[] += {
             QGVAR(bloodVolumeIndicator),
             QGVAR(stateIndicator1),
